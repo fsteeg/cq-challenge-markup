51d23c501702bd0affd5a052d0ec366f298e59d4
Fabian Steeg <fsteeg@gmail.com>
2010-09-05 21:24:32 +0200

    Basic internal representation and XML output

--- src/main/scala/MarkupParser.scala
+++ src/main/scala/MarkupParser.scala
@@ -4,11 +4,36 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import scala.util.parsing.combinator.RegexParsers
 import scala.util.parsing.combinator.JavaTokenParsers
+import scala.xml.Node
 
 class MarkupParser extends JavaTokenParsers with RegexParsers {
   override def skipWhitespace = false
-  def markup: Parser[List[String]] = rep(line) ^^ { case lines => lines.map(_._1) }
-  def line: Parser[(String, String)] = content ~ newline ^^ {case c ~ n => (c.toString, n.toString)}
-  def content: Parser[Any] = opt(ident)
+  def markup: Parser[Body] = body ^^ { case b => Body(b)}
+  def body: Parser[List[Element]] = rep(para|blankline) ^^ { 
+    case children => children.filter(_.isInstanceOf[Element]).map(_.asInstanceOf[Element])
+  }
+  def para: Parser[Para] = text ~ newline ^^ {case c~n => Para(List(c))}
+  def text: Parser[Text] = rep1(contentChar) ^^ {case chars => Text(chars.mkString.trim)}
+  def blankline: Parser[String] = newline ^^ { case n => "[blank]" }
+  def line: Parser[String] = text ~ newline ^^ { case c ~ n1 => c.toString }
+  def contentChar: Parser[Any] = """[a-z]""".r | " "
   def newline: Parser[Any] = "\u000D\u000A" | "\u000D" | "\u000A"
+}
+
+abstract class Element(children:List[Element]) {
+  override def toString = this match {
+    case Text(c) => "\"%s\"".format(c)
+    case _ => "(:%s %s)".format(getClass.getSimpleName, children.mkString(" "))
+  }
+  def toXml:Node
+}
+
+case class Body(children:List[Element]) extends Element(children) {
+  override def toXml = <body>{children.map(_.toXml)}</body>
+}
+case class Para(children:List[Element]) extends Element(children) {
+  override def toXml = <p>{children.map(_.toXml)}</p>
+}
+case class Text(content:String) extends Element(Nil){
+  override def toXml = new scala.xml.Text(content)
 }
--- src/test/scala/MarkupParserSpec.scala
+++ src/test/scala/MarkupParserSpec.scala
@@ -1,3 +1,4 @@
+import scala.xml.PrettyPrinter
 import scala.util.parsing.combinator.Parsers
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
@@ -6,33 +7,52 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class MarkupParserSpec extends MarkupParser with Spec with ShouldMatchers {
-  
+
   describe("The Markup language") {
-    it("uses CR (U+000D), CR/LF, (U+000D U+000A), or LF (U+000A) for line termination"){
-      expect(classOf[Success[Any]]){ parseAll(newline, "\u000D").getClass }
-      expect(classOf[Success[Any]]){ parseAll(newline, "\u000D\u000A").getClass }
-      expect(classOf[Success[Any]]){ parseAll(newline, "\u000A").getClass }
+    it("uses CR (U+000D), CR/LF, (U+000D U+000A), or LF (U+000A) for line termination") {
+      expect(classOf[Success[Any]]) { parseAll(newline, "\u000D").getClass }
+      expect(classOf[Success[Any]]) { parseAll(newline, "\u000D\u000A").getClass }
+      expect(classOf[Success[Any]]) { parseAll(newline, "\u000A").getClass }
     }
   }
   
-  describe("The Markup parser") {
-    it("can parse markup input into a tree structure") {
-      expect(
-          List(
-              None, 
-              Some("first"), 
-              Some("second"), 
-              None, 
-              Some("more")
-              ).toString
-      ){parseAll(markup,
+  val input = 
 """
-first
-second
+a first paragraph
 
-more
+and another
 """
-      ).get.toString
-    }}
+
+  describe("The Markup parser") {
+    it("can parse markup input into an internal tree representation") {
+      expect(
+       Body(List(
+           Para(List(
+                Text("a first paragraph"))), 
+            Para(List(
+                Text("and another")))))
+        ) {
+        parseAll(markup,input).get
+      }
+    }
+    
+    it("can display the parsed structure as a concise string representation") {
+      expect("""(:Body (:Para "a first paragraph") (:Para "and another"))""") {
+        val res = parseAll(markup,input).get
+        println(res)
+        res.toString
+      }
+    }
+    
+    it("can transform the parsed structure to an XML representation") {
+      val pretty = new PrettyPrinter(200,2)
+      val xml = <body><p>a first paragraph</p><p>and another</p></body>
+      expect(pretty format xml) {
+        val res = parseAll(markup,input).get
+        println(pretty format res.toXml)
+        pretty format res.toXml
+      }
+    }
+    
   }
 }
